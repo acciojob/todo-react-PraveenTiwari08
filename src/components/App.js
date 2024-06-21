02f2178a789126c57bf91836d28252303c1b60a5
PraveenTiwari08 <praviintiwari8@gmail.com>
2024-06-21 21:31:20 +0530

    first

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,33 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [Todos, setTodos] = useState([])
+  const [inputValue, setInputValue] = useState("");
+
+  const inputvalue = (e) => {
+    setInputValue(e.target.value);
+  }
+
+  const addTodo = () =>{
+    setTodos([...Todos,inputValue])
+    setInputValue("");
+  }
+  
+  const todoAddInLi = Todos.map((value,index)=>{
+      return <div><li>{value}</li></div>
+  })
   return (
     <div>
-        {/* Do not remove the main div */}
+       <h1>todo-list</h1>
+       <input value={inputValue} onChange={inputvalue}/>
+       <button onClick={addTodo}>Add Task</button>
+       <ul>
+        <li>
+          {todoAddInLi}
+        </li>
+       </ul>
     </div>
   )
 }
